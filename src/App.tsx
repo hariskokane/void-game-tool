bc1dc56e71c903be20038cf0a97d5dbdb884dba7
Haris Kokane <hariskokane0@gmail.com>
2025-10-16 23:13:22 +0530

    Add route for ExpPerStam page

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Home from "./pages/Home";
 import LevelCalculator from "./pages/LevelCalculator";
 import PlayerCalculator from "./pages/PlayerCalculator";
 import PetCalculator from "./pages/PetCalculator";
+import ExpPerStam from "./pages/ExpPerStam";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -30,6 +31,7 @@ const App = () => (
             <Route path="/level-calculator" element={<LevelCalculator />} />
             <Route path="/player-calculator" element={<PlayerCalculator />} />
             <Route path="/pet-calculator" element={<PetCalculator />} />
+            <Route path="/exp-per-stam" element={<ExpPerStam />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
