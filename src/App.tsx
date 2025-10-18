837f0f155d0e40d7fcfce1a7717c43fa12b165f1
Haris Kokane <hariskokane0@gmail.com>
2025-10-18 11:03:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Analytics } from "@vercel/analytics/react";
 import FspDamage from "@/pages/FspDamage";
 import FspNeeded from "@/pages/FspNeeded";
 import StaminaRegen from "@/pages/StaminaRegen";
@@ -27,6 +28,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+          <Analytics />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/damage-per-fsp" element={<FspDamage />} />
